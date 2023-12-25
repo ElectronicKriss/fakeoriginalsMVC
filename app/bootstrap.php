ed0949a870e9618f7b4dae3cf7e4911b8ec3e099
Emmanuel Kristiyefunmi <ayokristiyefunmi@gmail.com>
2023-12-25 17:11:51 +0100

    v1

---
+++ app/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    // Load Config
+
+    require_once 'config/config.php';
+
+    
+    // Autoload Libraries
+    
+    spl_autoload_register(function ($className){
+
+        require_once 'libraries/' . $className . '.php';
+    });
+
+    // Init Core Library
+
+    $init = new Core;
---
+++ app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1><?php echo $data['title']; ?></h1>
+<p>This is the fakeOriginalsMVC PHP Framework, please refer to the docs for detailed info.</p>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
